e5d37b242e624f57d8d8e60aaa4055af8c3e26a9
hiramh <hiramh@cdc-d-hiramh.synnex.org>
2014-12-04 11:36:57 +0800

    update the hibernet Configuration information and entity

---
+++ src/main/java/com/synnex/model/Dictionary.java
@@ -0,0 +1,26 @@
+package com.synnex.model;
+
+public class Dictionary {
+	private int id;
+	private String category;
+	private String value;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+}
