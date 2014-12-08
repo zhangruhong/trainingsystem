9bd46a14bfc470c4bbd102e10a121a0b7666006f
Davisz <Davisz@cdc-d-davisz-w7.synnex.org>
2014-12-08 13:51:34 +0800

    完成了UserService的实现类UserServiceImpl的初步编写

--- src/main/java/com/synnex/service/UserService.java
+++ src/main/java/com/synnex/service/UserService.java
@@ -2,30 +2,76 @@ package com.synnex.service;
 
 import java.util.List;
 
+import com.synnex.dao.Order;
 import com.synnex.model.User;
 
-
 public interface UserService {
-/**
- * ----是否应该添加个类或者修改大分类类来对学员进行分组？
- * 包括用户的（单个、批量）增删改查-管理员专属
- * 包括用户个人信息的修改（自己修改）
- * 包括用户自己修改密码
- * 
- */
+	/**
+	 * ----是否应该添加个类或者修改大分类类来对学员进行分组？ 包括用户的（单个、批量）增删改查-管理员专属 包括用户自己修改密码
+	 * 
+	 */
+	/**
+	 * 新建一个User对象
+	 * 
+	 * @param user
+	 */
 	public void addUser(User user);
-	
+
+	/**
+	 * 批量新建User对象（应用场景-讲师输入学员名字批量建用户并发邮件通知）
+	 * 
+	 * @param users
+	 */
 	public void addUsers(List<User> users);
-	
+
+	/**
+	 * 删除/冻结一个用户（应用场景-培训学员离职，或者不再参加培训等）
+	 * 
+	 * @param user
+	 */
 	public void deleteUser(User user);
-	
+
+	/**
+	 * 批量删除/冻结/转存一批用户（应用场景-一批学员毕业）
+	 * 
+	 * @param users
+	 */
 	public void deleteUsers(List<User> users);
-	
+
+	/**
+	 * 更新一个学员的信息(讲师维护)
+	 * 
+	 * @param user
+	 */
 	public void updateUser(User user);
-	
+
+	/**
+	 * 批量更新学员信息（应用场景-新建学员时勾选多个然后一起分配到某一部门）
+	 * 
+	 * @param users
+	 */
 	public void updateUsers(List<User> users);
-	
+
+	/**
+	 * 根据编号查学员
+	 * 
+	 * @param id
+	 *            （此编号为系统内部编号，对所有人透明）
+	 * @return
+	 */
 	public User getUser(int id);
-	
-	public List<User> getUsers();
+
+	/**
+	 * 
+	 * @param condition
+	 *            查询条件 被赋值的当前类
+	 * @param orders
+	 *            排列方式
+	 * @param begin
+	 *            开始页面
+	 * @param size
+	 *            步长
+	 * @return
+	 */
+	public List<User> getUsersByCondition(User condition, List<Order> orders, int begin, int size);
 }
---
+++ src/main/java/com/synnex/service/UserServiceImpl.java
@@ -0,0 +1,63 @@
+package com.synnex.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Repository;
+
+import com.synnex.dao.Order;
+import com.synnex.dao.UserDao;
+import com.synnex.model.User;
+
+@Repository
+public class UserServiceImpl implements UserService {
+
+	@Resource(name = "userDaoImpl")
+	private UserDao userDao;
+
+	@Override
+	public void addUser(User user) {
+		userDao.save(user);
+	}
+
+	@Override
+	public void addUsers(List<User> users) {
+		userDao.save(users);
+	}
+
+	@Override
+	public void deleteUser(User user) {
+		userDao.delete(user);
+	}
+
+	@Override
+	public void deleteUsers(List<User> users) {
+		// TODO 有待优化
+		for (User user : users) {
+			this.deleteUser(user);
+		}
+	}
+
+	@Override
+	public void updateUser(User user) {
+		userDao.update(user);
+
+	}
+
+	@Override
+	public void updateUsers(List<User> users) {
+		userDao.update(users);
+	}
+
+	@Override
+	public User getUser(int id) {
+		return userDao.get(id);
+	}
+
+	@Override
+	public List<User> getUsersByCondition(User condition, List<Order> orders, int begin, int size) {
+		return userDao.list(condition, orders, begin, size);
+	}
+
+}
