30acb27357a3d43402039a8e1b6b0456f15ce93a
Davisz <Davisz@cdc-d-davisz-w7.synnex.org>
2014-12-11 11:10:53 +0800

    处理了部分Term和Usergroup的Dao

---
+++ src/main/java/com/synnex/dao/impl/TermDaoImpl.java
@@ -0,0 +1,7 @@
+package com.synnex.dao.impl;
+
+import com.synnex.dao.TermDao;
+import com.synnex.model.Term;
+
+public class TermDaoImpl extends GenericDaoImpl<Term, Integer> implements TermDao {
+}
