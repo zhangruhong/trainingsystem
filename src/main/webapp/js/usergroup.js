0bdec18dcaa34d5ec815a59575c53c0615e1893c
zhangruhong168 <zhangruhong@foxmail.com>
2014-12-21 23:48:05 +0800

    1.完成了部分获取组员信息的js代码的编写——还须提取填充方法给其他函数公用

--- src/main/webapp/js/usergroup.js
+++ src/main/webapp/js/usergroup.js
@@ -1,5 +1,6 @@
 function addTerm() {
-	var mydata = '{"name":"' + $("#nameInput").val() + '","description":"' + $('#descriptionInput').val() + '"}';
+	var mydata = '{"name":"' + $("#nameInput").val() + '","description":"'
+			+ $('#descriptionInput').val() + '"}';
 	$
 			.ajax({
 				type : "POST",
@@ -16,9 +17,14 @@ function addTerm() {
 								.html(
 										"<div class='alert alert-success alert-dismissable'> <button type='button' class='close' data-dismiss='alert'  aria-hidden='true'>&times;</button>数据添加成功！</div>");
 					} else {
-						$("#actiontip").html(
-								"<div class='alert alert-warning alert-dismissable'> <button type='button' class='close' data-dismiss='alert'  aria-hidden='true'>&times;</button>"
-										+ data.msg + '<br/>' + terms.name + '<br/>' + terms.description + "</div>");
+						$("#actiontip")
+								.html(
+										"<div class='alert alert-warning alert-dismissable'> <button type='button' class='close' data-dismiss='alert'  aria-hidden='true'>&times;</button>"
+												+ data.msg
+												+ '<br/>'
+												+ terms.name
+												+ '<br/>'
+												+ terms.description + "</div>");
 					}
 				},
 				error : function(XMLHttpRequest, textStatus, errorThrown) {
@@ -31,19 +37,53 @@ function addTerm() {
 function createShowingTable(data) {
 	// 此处需要让其动态的生成一个table并填充数据
 	var tableStr = "";
-	$.each(data, function(n, term) {
-		var termstring = "<tr><td>" + term.id + "</td>" + "<td>" + term.name + "</td>" + "<td>" + term.description + "</td>" + "<td>" + '功能待定'
-				+ "</td>" + "<td><a href='" + '#连接到编辑页面（暂不支持）' + " class='btn btn-default'>" + 'Edit' + "</a></td>" + "<td><a href='" + '#连接到删除页面'
-				+ " class='btn btn-link'>" + 'Delete' + "</a></td>" + "<td><a href='" + "href111" + " class='btn btn-default'>" + '组员管理'
-				+ "</a></td>" + "<td>" +
-				// <!-- Split button -->
-				"<div class='btn-group'>" + "<button type='button' class='btn btn-info'>Action</button>"
-				+ "<button type='button' class='btn btn-info dropdown-toggle' data-toggle='dropdown'>"
-				+ "<span class='caret'></span> <span class='sr-only'>Toggle Dropdown</span> </button>" + "<ul class='dropdown-menu' role='menu'>"
-				+ "<li><a href='" + '#功能待定' + "'>Bootstrap</a></li>'" + "<li><a href='" + '#功能待定' + "'>Bootstrap</a></li>'"
-				+ "<li><a onclick='deleteUserGroup(" + term.id + ")'>删除/解散该组</a></li>'" + "</ul> </div> </td>" + "</tr>";
-		tableStr = tableStr + termstring;
-	});
+	$
+			.each(
+					data,
+					function(n, term) {
+						var termstring = "<tr><td>"
+								+ term.id
+								+ "</td>"
+								+ "<td>"
+								+ term.name
+								+ "</td>"
+								+ "<td>"
+								+ term.description
+								+ "</td>"
+								+ "<td>"
+								+ '功能待定'
+								+ "</td>"
+								+ "<td><a href='"
+								+ '#连接到编辑页面（暂不支持）'
+								+ " class='btn btn-default'>"
+								+ 'Edit'
+								+ "</a></td>"
+								+ "<td><a href='"
+								+ '#连接到删除页面'
+								+ " class='btn btn-link'>"
+								+ 'Delete'
+								+ "</a></td>"
+								+ "<td><a href='"
+								+ "href111"
+								+ " class='btn btn-default'>"
+								+ '组员管理'
+								+ "</a></td>"
+								+ "<td>"
+								+
+								// <!-- Split button -->
+								"<div class='btn-group'>"
+								+ "<button type='button' class='btn btn-info'>Action</button>"
+								+ "<button type='button' class='btn btn-info dropdown-toggle' data-toggle='dropdown'>"
+								+ "<span class='caret'></span> <span class='sr-only'>Toggle Dropdown</span> </button>"
+								+ "<ul class='dropdown-menu' role='menu'>"
+								+ "<li><a href='" + '#功能待定'
+								+ "'>Bootstrap</a></li>'" + "<li><a href='"
+								+ '#功能待定' + "'>Bootstrap</a></li>'"
+								+ "<li><a onclick='deleteUserGroup(" + term.id
+								+ ")'>删除/解散该组</a></li>'" + "</ul> </div> </td>"
+								+ "</tr>";
+						tableStr = tableStr + termstring;
+					});
 	// 将动态生成的table添加的事先隐藏的div中.
 	$("#tbodyusergroup").html(tableStr);
 }
@@ -60,13 +100,15 @@ function deleteUserGroup(groupid) {
 					var terms = data.terms;
 					if (data.success == true) {
 						createShowingTable(terms);
-						$("#actiontip").html(
-								"<div class='alert alert-success alert-dismissable'> <button type='button' class='close' data-dismiss='alert'  aria-hidden='true'>&times;</button>"
-										+ data.msg + "</div>");
+						$("#actiontip")
+								.html(
+										"<div class='alert alert-success alert-dismissable'> <button type='button' class='close' data-dismiss='alert'  aria-hidden='true'>&times;</button>"
+												+ data.msg + "</div>");
 					} else {
-						$("#actiontip").html(
-								"<div class='alert alert-warning alert-dismissable'> <button type='button' class='close' data-dismiss='alert'  aria-hidden='true'>&times;</button>"
-										+ data.msg + "</div>");
+						$("#actiontip")
+								.html(
+										"<div class='alert alert-warning alert-dismissable'> <button type='button' class='close' data-dismiss='alert'  aria-hidden='true'>&times;</button>"
+												+ data.msg + "</div>");
 					}
 				},
 				error : function(XMLHttpRequest, textStatus, errorThrown) {
@@ -79,6 +121,7 @@ function deleteUserGroup(groupid) {
 
 function setgroupidvalue(groupid) {
 	$("#groupidvalue").text(groupid);
+	loadUsersOfGroup();
 }
 
 function addUserToGroup() {
@@ -97,12 +140,14 @@ function addUserToGroup() {
 				alert("成功！" + data.msg);
 				// createShowingTable(terms);
 				// $("#actiontip").html(
-				// "<div class='alert alert-success alert-dismissable'> <button type='button' class='close' data-dismiss='alert'
+				// "<div class='alert alert-success alert-dismissable'> <button
+				// type='button' class='close' data-dismiss='alert'
 				// aria-hidden='true'>&times;</button>"
 				// + data.msg + "</div>");
 			} else {
 				// $("#actiontip").html(
-				// "<div class='alert alert-warning alert-dismissable'> <button type='button' class='close' data-dismiss='alert'
+				// "<div class='alert alert-warning alert-dismissable'> <button
+				// type='button' class='close' data-dismiss='alert'
 				// aria-hidden='true'>&times;</button>"
 				// + data.msg + "</div>");
 				alert("失败" + data.msg);
@@ -111,7 +156,8 @@ function addUserToGroup() {
 		error : function(XMLHttpRequest, textStatus, errorThrown) {
 			// $("#actiontip")
 			// .html(
-			// "<div class='alert alert-danger alert-dismissable'> <button type='button' class='close' data-dismiss='alert'
+			// "<div class='alert alert-danger alert-dismissable'> <button
+			// type='button' class='close' data-dismiss='alert'
 			// aria-hidden='true'>&times;</button>网络或兼容性错误！删除失败！请练习davisz@synnex.com。</div>");
 			alert("错误");
 		}
@@ -119,5 +165,35 @@ function addUserToGroup() {
 }
 
 function loadUsersOfGroup() {
+	var groupidvalue = $("#groupidvalue").text();
+	var url = groupidvalue + "/users";
+	$.getJSON(url,function(data,status,xhr){
+		if(status=="success"){
+			data=data.termmap
+			if(data.success==true){
+				data=data.terms;
+				var trvar = $("<tr></tr>");
+				$.each(data,function(index,term){
+					var tdvar = $("<td></td>");
+					var tdvar1 = tdvar.clone().text(term.id);
+					var tdvar2 = tdvar.clone().text(term.loginname);
+					var tdvar3 = tdvar.clone().text(term.email);
+					var tdvar4 = tdvar.clone().text(term.phoneno);
+					tdvar = tdvar.append(tdvar1, tdvar2, tdvar3,tdvar4);
+					trvar = trvar.append(tdvar);
+				})
+				$("#show_users_of_group").html(trvar);
+			}else{
+				alert("没有获取到记录"+data.msg)
+			}
+		}else{
+			//标识网络连接状态没有成功
+			alert("status:"+status+"data:"+data);
+		}
+		
+	})
+		
+		
+		
 	
 }
